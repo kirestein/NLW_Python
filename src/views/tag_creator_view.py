e70aae1d06f1a99252a161cafd5337aa82900fbb
Erik <erik.proenca2011@gmail.com>
2024-02-12 15:06:24 -0300

    feat: Implementing view for tag creator with http type

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,15 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView():
+    '''
+        responsability for interacting with HTTP
+    '''
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        product_code = body["product_code"]
+        
+        # Logica de regra de negócio
+        
+        #retorno http
+        return HttpResponse(status_code=200, body={"erik": "proença"})
